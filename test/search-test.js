7197046f83e03c00fb03e361602c3feba6a5f4ae
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-06-11 03:19:01 +0100

    Update test script

--- test/search-test.js
+++ test/search-test.js
@@ -1,61 +1,33 @@
-var mocha = require('mocha');
-var should = require('should');
 var books = require('../lib/google-books-search.js');
+var chai = require('chai');
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var should = require('chai').should();
 
 describe('Searching', function() {
 
-	it('should return a JSON object of books', function(done) {
-		books.search('Guinness World Records', {}, function(error, results) {
-			should.not.exist(error);
-			should.exist(results);
-			results[0].should.have.property('title');
-			done();
-		});
+	it('the options argument should be optional', function() {
+		return books.search('Guinness World Records').should.be.fulfilled;
 	});
 
-	it('the options argument should be optional', function(done) {
-		books.search('Guinness World Records', function(error, results) {
-			should.not.exist(error);
-			should.exist(results);
-			results[0].should.have.property('title');
-			done();
-		});
+	it('should return an empty object if there are no results', function() {
+		return books.search('JCEhrrpxF2E1s7aPW8zd2903tQ4AlCB9', {}).should.eventually.be.empty;
 	});
 
-	it('should return an empty object if there are no results', function(done) {
-		books.search('JCEhrrpxF2E1s7aPW8zd2903tQ4AlCB9', {}, function(error, results) {
-			should.not.exist(error);
-			should.exist(results);
-			results.length.should.equal(0);
-			done();
-		});
-	});
-
-	it('should return a specified number of results', function(done) {
-		books.search('Guinness World Records', {
+	it('should return a specified number of results', function() {
+		return books.search('Guinness World Records', {
 			limit: 15
-		}, function(error, results) {
-			should.not.exist(error);
-			results.length.should.equal(15);
-			done();
-		});
+		}).should.eventually.have.length(15);
 	});
 
-	it('should only accept an limit below 40', function(done) {
-		books.search('Guinness World Records', {
+	it('should only accept an limit below 40', function() {
+		return books.search('Guinness World Records', {
 			limit: 50
-		}, function(error, results) {
-			should.exist(error);
-			done();
-		});
+		}).should.be.rejected;
 	});
 
-	it('should return an error if no query is specified', function(done) {
-		books.search(null, {}, function(error, results) {
-			should.exist(error);
-			should.not.exist(results);
-			done();
-		});
+	it('should return an error if no query is specified', function() {
+		return books.search(null, {}).should.be.rejected;
 	});
 
 });
